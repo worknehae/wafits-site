007a3af69389c3a78174bd6aeef9b9f88aa1686f
Workneh Emrie <workneh.emrie@trade.gov>
2025-05-28 13:55:34 -0400

    2nd Update site content

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.wafits a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
